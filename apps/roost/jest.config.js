d6accec3f66958942c74f9712511a0a016c5eeb5
Kyle Banks <Kyle.Ryan.Banks@gmail.com>
2022-04-22 00:06:05 -0500

    nx g nx-electron:app roost --frontendProject=familiar

---
+++ apps/roost/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'roost',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/roost',
+};
