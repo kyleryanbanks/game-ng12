bfa58dd8959f2efd0c03bad6f34b009334419f78
Kyle Banks <Kyle.Ryan.Banks@gmail.com>
2022-05-03 22:46:06 -0500

    wire up form controller

--- libs/ui/src/lib/form-controller/form-controller.component.ts
+++ libs/ui/src/lib/form-controller/form-controller.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { DS4_BUTTONS, DS4_DPAD_DIRECTIONS } from '@game-ng12/controller';
 
 @Component({
@@ -10,4 +11,47 @@ import { DS4_BUTTONS, DS4_DPAD_DIRECTIONS } from '@game-ng12/controller';
 export class FormControllerComponent {
   _DIR = DS4_DPAD_DIRECTIONS;
   _BTN = DS4_BUTTONS;
+  controller: FormGroup;
+  lastFrame = {};
+
+  constructor(fb: FormBuilder) {
+    this.controller = fb.group({
+      direction: [8],
+      buttons: fb.group({
+        THUMB_RIGHT: [false],
+        THUMB_LEFT: [false],
+        OPTIONS: [false],
+        SHARE: [false],
+        TRIGGER_RIGHT: [false],
+        TRIGGER_LEFT: [false],
+        SHOULDER_RIGHT: [false],
+        SHOULDER_LEFT: [false],
+        TRIANGLE: [false],
+        CIRCLE: [false],
+        CROSS: [false],
+        SQUARE: [false],
+      }),
+    });
+  }
+
+  onSubmit() {
+    this.lastFrame = this.controller.value;
+    this.controller.reset({
+      direction: 8,
+      buttons: {
+        THUMB_RIGHT: false,
+        THUMB_LEFT: false,
+        OPTIONS: false,
+        SHARE: false,
+        TRIGGER_RIGHT: false,
+        TRIGGER_LEFT: false,
+        SHOULDER_RIGHT: false,
+        SHOULDER_LEFT: false,
+        TRIANGLE: false,
+        CIRCLE: false,
+        CROSS: false,
+        SQUARE: false,
+      },
+    });
+  }
 }
