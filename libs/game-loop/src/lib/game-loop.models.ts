eebd42b8bf1c590a2338202971b996ad9c0212b3
Kyle Banks <Kyle.Ryan.Banks@gmail.com>
2022-05-14 01:08:51 -0500

    update models

--- libs/game-loop/src/lib/game-loop.models.ts
+++ libs/game-loop/src/lib/game-loop.models.ts
@@ -16,7 +16,18 @@ export interface Buttons {
   STONE: boolean;
 }
 
+export enum MVCIMap {
+  LK = 0,
+  HK = 1,
+  LP = 2,
+  HP = 3,
+  STONE = 4,
+  TAG = 5,
+}
+
 export interface Frame {
-  direction: number;
-  buttons: Buttons;
+  frame: number;
+  hold?: number;
+  cardinalDirection: number; // cardinal directions. 0 = up incrementing clockwise.
+  buttons: number; // Bitfield of all gamepad APIs buttons pressed state
 }
