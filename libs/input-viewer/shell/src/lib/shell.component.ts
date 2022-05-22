ece461ae7de62450e7cc93fe24227eb0e5c510c7
Kyle Banks <Kyle.Ryan.Banks@gmail.com>
2022-05-22 16:32:13 -0500

    Update shell to display a viewer for each connected gamepad

--- libs/input-viewer/shell/src/lib/shell.component.ts
+++ libs/input-viewer/shell/src/lib/shell.component.ts
@@ -1,8 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { GameLoopService } from '@game-ng12/game-loop';
+import { Subscription } from 'rxjs';
 
 @Component({
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ShellComponent {}
+export class ShellComponent implements OnInit, OnDestroy {
+  subscription = new Subscription();
+  order = [0, 1, 2, 3];
+  connectedGamepads: { [id: string]: boolean } = {};
+
+  constructor(public gameLoop: GameLoopService) {}
+
+  ngOnInit() {
+    this.subscription.add(
+      this.gameLoop.connected$.subscribe((event) => {
+        this.connectedGamepads[event.gamepad.id] = true;
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
